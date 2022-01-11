26073c5d20b4bd18192f1768fc98666375bc461c
Franciele B. de Oliveira <32960110+fran-olv@users.noreply.github.com>
2022-01-11 00:14:54 -0300

    Add files via upload

---
+++ exemplo.js
@@ -0,0 +1,9 @@
+function ola(){
+    var xss = 0
+    
+    console.log(3xss+1);
+    console.log3(3xss+1);
+    console.log(3xss+1);
+
+asdasdsdasddddddddddddddddddddddddddddddddddddddddddddddasddddddddddddddddddddddddddddddddddddd asdasdasdasdasdasdasdasdasd      fdsasdasdasddfee
+}
